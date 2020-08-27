227a6140d477cc8422135ce5200780e23a8700f7
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-08-27 15:09:55 +0500

    Data types practice (int, str, float & complex)

---
+++ LAB 2/datatypes.py
@@ -0,0 +1,15 @@
+x = "abdullah maroof"
+y = 20
+z = 12.76
+a = 12j
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+b = y + z
+print(b)
+print(type(b))
+c = b+a
+print(c)
+print(type(c))
+
