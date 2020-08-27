8c9ad42e008f9360990d4fc8d01cdbb8b0e83649
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-08-27 16:10:22 +0500

    Students Grades

---
+++ LAB 2/GRADES.py
@@ -0,0 +1,8 @@
+#STUDENTS GRADES
+name = ["Abdullah ", "Aneesa ", "Shifa", "Akmal", "Nisar", "Bushra ", "Hafsa"]
+marks= ["   410   ", "  390  ", " 430 ", " 370 ", " 380 ", "  350  ", " 320 "]
+print("                          Students Grades Record")
+print("                          ----------------------")
+print("Total Marks = 450")
+print("Names : ",name)
+print("Marks : ",marks)
