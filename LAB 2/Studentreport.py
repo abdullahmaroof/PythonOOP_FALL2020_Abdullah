4ad9e58254e4f78748dd0721363208f65523ee19
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-08-27 15:53:01 +0500

    Students record

---
+++ LAB 2/Studentreport.py
@@ -0,0 +1,9 @@
+# 3 students data
+print("                                          Student Data")
+print("                                          ************")
+stu_1 = {"name" : "Abdullah Maroof", "roll no" : "BAIM-S20-001", "Email" : "baim-s20-001@superior.edu.pk", "Grade" : "A"}
+stu_2 = {"name" : " Aneesa Ashraf ", "roll no" : "BAIM-S20-002", "Email" : "baim-s20-002@superior.edu.pk", "Grade" : "B"}
+stu_3 = {"name" : " Shifa Rafique ", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "C"}
+print(stu_1)
+print(stu_2)
+print(stu_3)
