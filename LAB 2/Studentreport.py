9d418c336657c5bb6231e58e39e3a234d143e46f
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-08-27 15:59:23 +0500

    Students record

--- LAB 2/Studentreport.py
+++ LAB 2/Studentreport.py
@@ -1,9 +1,23 @@
-# 3 students data
+# 10 students data
 print("                                          Student Data")
 print("                                          ************")
-stu_1 = {"name" : "Abdullah Maroof", "roll no" : "BAIM-S20-001", "Email" : "baim-s20-001@superior.edu.pk", "Grade" : "A"}
-stu_2 = {"name" : " Aneesa Ashraf ", "roll no" : "BAIM-S20-002", "Email" : "baim-s20-002@superior.edu.pk", "Grade" : "B"}
-stu_3 = {"name" : " Shifa Rafique ", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "C"}
+stu_1 = {"name" : "Abdullah Maroof", "roll no" : "BAIM-S20-001", "Email" : "baim-s20-001@superior.edu.pk", "Grade" : "A+"}
+stu_2 = {"name" : " Aneesa Ashraf ", "roll no" : "BAIM-S20-002", "Email" : "baim-s20-002@superior.edu.pk", "Grade" : "B+"}
+stu_3 = {"name" : " Shifa Rafique ", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "A-"}
+stu_4 = {"name" : "  Nisar Akram  ", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "C+"}
+stu_5 = {"name" : " Usamah Ghulam ", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "D-"}
+stu_6 = {"name" : " Bushra  Akram ", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "A+"}
+stu_7 = {"name" : "  Abdul  Hadi  ", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "B-"}
+stu_8 = {"name" : "Daniyal  Gulzar", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "A-"}
+stu_9 = {"name" : " Hafsa  Masood ", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "C-"}
+stu_10 = {"name": "Naomi Dee Marco", "roll no" : "BAIM-S20-003", "Email" : "baim-s20-003@superior.edu.pk", "Grade" : "B+"}
 print(stu_1)
 print(stu_2)
 print(stu_3)
+print(stu_4)
+print(stu_5)
+print(stu_6)
+print(stu_7)
+print(stu_8)
+print(stu_9)
+print(stu_10)
