ed070f1931ce7c1f2ff395a9ec793dcb1a327cd1
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-01 20:50:00 +0500

    conditions(if, if else)

--- LEC 3/Conditions.py
+++ LEC 3/Conditions.py
@@ -6,3 +6,11 @@ marks = 80
 if marks >=50:
     print("Ali is pass")
     print(marks," out of 100")
+print("----------------------")
+
+grade = "B+"
+if grade == "A+" or grade == "A-":
+    print("Ali is pass")
+else:
+    print("Ali is fail")
+
