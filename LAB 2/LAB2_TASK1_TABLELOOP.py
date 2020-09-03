0447ed3762189cfac03e96628c1e8845dcc3c469
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-03 08:27:44 +0500

    table in loops

---
+++ LAB 2/LAB2_TASK1_TABLELOOP.py
@@ -0,0 +1,22 @@
+# table with loops
+
+# tbale with while loop
+
+print("*********Table with While loop***********")
+
+table = 8
+i = 1
+while i<=10:
+    x = table * i
+    print(table," * ",i," = ",x)
+    i += 1
+print("-------------------------------------------\n")
+
+# table with for loop
+
+print("*********Table with for loop******************")
+
+table = 5
+for i in range(1,11):
+    x=table*i
+    print(table," * ",i," = ",x)
