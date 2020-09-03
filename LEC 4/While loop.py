02328a0220d4de69a99fcc4c208ac032e30b06d7
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-03 08:16:17 +0500

    While loop

--- LEC 4/While loop.py
+++ LEC 4/While loop.py
@@ -5,7 +5,7 @@ while x<=10:
     i = 2*x
     print(i)
     x+=1
-print("-------------------------------------------")
+print("-------------------------------------------\n")
 
 #table by using whilw loop
 
@@ -14,4 +14,18 @@ i = 1
 while i<=10:
     x = table * i
     print(table," * ",i," = ",x)
+    i += 1
+print("-------------------------------------------\n")
+
+#nested while loop
+
+h = 20
+i = 1
+while i<=4:
+    j=1
+    print("_____________________")
+    while j<=5:
+        print(h)
+        h +=5
+        j +=1
     i += 1
