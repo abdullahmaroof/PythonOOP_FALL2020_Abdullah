8098627f6d42971752a354160d7d1a448bd4a195
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-03 09:46:28 +0500

    even and odd numbers

--- LAB 2/Lab2_task2_odd&even.py
+++ LAB 2/Lab2_task2_odd&even.py
@@ -1,43 +1,10 @@
-# Taking num from user to get even and odd
+# Taking num from user to check even and odd
 
-# by for loop even number
-print("even number by using for loop")
-lowerrang = int(input("Enter starting range: "))
-upperrang = int(input("Enter ending range: "))
-upperrang += 1
-for evennum in range(lowerrang,upperrang):
-    if evennum % 2 == 0:
-        print(evennum)
-print("\n---------------------------\n")
+#check even number
+print("check number enetered by user is even or odd ")
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("The number enetered by user is even and number is ",num)
+else:
+    print("The number enetered by user is odd and number is ", num)
 
-# by for loop odd number
-print("odd number by using for loop")
-lowerrang = int(input("Enter starting range: "))
-upperrang = int(input("Enter ending range: "))
-upperrang += 1
-for oddnnum in range(lowerrang,upperrang):
-    if oddnnum % 2 != 0:
-        print(oddnnum)
-print("\n---------------------------\n")
-
-# by while loop even number
-print("even number by using whileloop")
-lowerrang = int(input("Enter starting range: "))
-upperrang = int(input("Enter ending range: "))
-
-while lowerrang<=upperrang:
-    if lowerrang % 2 == 0:
-        print(lowerrang)
-    lowerrang += 1
-print("\n---------------------------\n")
-
-# by while loop odd number
-print("odd number by while for loop")
-lowerrang = int(input("Enter starting range: "))
-upperrang = int(input("Enter ending range: "))
-
-while lowerrang<=upperrang:
-    if lowerrang % 2 != 0:
-        print(lowerrang)
-    lowerrang += 1
-print("\n---------------------------\n")
