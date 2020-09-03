fc2c91776792328b86ef5d3a40c2c28802e1a733
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-03 17:39:14 +0500

    for loop

--- Lecture/lec4/For_loop.py
+++ Lecture/lec4/For_loop.py
@@ -14,4 +14,10 @@ print("------------------------------------\n")
 
 #range from user
 tablenum = int(input("Enter a number: "))
-lowerrange = int(input("Enter a number from where u want to start ur table: "))
+lower_range = int(input("Enter a number from where u want to start your table: "))
+upper_range = int(input("Enter a number from where u want to end your table: "))
+for table in range(lower_range,upper_range + 1):
+    ans = tablenum * table
+    print(tablenum, " * ", table, " = ", ans)
+print("------------------------------------\n")
+
