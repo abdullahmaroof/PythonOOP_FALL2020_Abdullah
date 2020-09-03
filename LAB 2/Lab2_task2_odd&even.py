6cb964066b9b9279704a3f96b2992b0587fc25c3
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-03 09:39:41 +0500

    even and odd numbers

--- LAB 2/Lab2_task2_odd&even.py
+++ LAB 2/Lab2_task2_odd&even.py
@@ -1,7 +1,7 @@
 # Taking num from user to get even and odd
 
 # by for loop even number
-
+print("even number by using for loop")
 lowerrang = int(input("Enter starting range: "))
 upperrang = int(input("Enter ending range: "))
 upperrang += 1
@@ -11,11 +11,33 @@ for evennum in range(lowerrang,upperrang):
 print("\n---------------------------\n")
 
 # by for loop odd number
-
+print("odd number by using for loop")
 lowerrang = int(input("Enter starting range: "))
 upperrang = int(input("Enter ending range: "))
 upperrang += 1
 for oddnnum in range(lowerrang,upperrang):
     if oddnnum % 2 != 0:
         print(oddnnum)
+print("\n---------------------------\n")
+
+# by while loop even number
+print("even number by using whileloop")
+lowerrang = int(input("Enter starting range: "))
+upperrang = int(input("Enter ending range: "))
+
+while lowerrang<=upperrang:
+    if lowerrang % 2 == 0:
+        print(lowerrang)
+    lowerrang += 1
+print("\n---------------------------\n")
+
+# by while loop odd number
+print("odd number by while for loop")
+lowerrang = int(input("Enter starting range: "))
+upperrang = int(input("Enter ending range: "))
+
+while lowerrang<=upperrang:
+    if lowerrang % 2 != 0:
+        print(lowerrang)
+    lowerrang += 1
 print("\n---------------------------\n")
