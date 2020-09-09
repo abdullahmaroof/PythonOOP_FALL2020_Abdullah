c47225f9498d974ee0044210e1cb4de804e949a7
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-09 18:56:04 +0500

    While loop for student list

--- Lecture/lec6/studentlist_in_loop.py
+++ Lecture/lec6/studentlist_in_loop.py
@@ -6,21 +6,19 @@ std_name = []
 std_father_name = []
 std_section = []
 std_dep = []
-condition = True
+press = 1
+condition = 0
 print("Student record system")
 print("-----------------------------------\n")
-while condition == True:
+while condition < press:
     std_name.append(input("Enter name of new student: "))
     std_father_name.append(input("Enter father name of new student: "))
     std_roll_no.append(input("Enter roll no of new student: "))
     std_section.append(input("Enter section of new student: "))
     std_dep.append(input("Enter department of new student: "))
-    condition = False
     print("---------------------------------------------------------\n")
     print("Do you want to enter a new record of student")
-    press = input("Press 1 to enter or press 0 to save record: ")
-    if press == 1:
-        condition == True
-    else:
-        condition == False
-print("\nRecord is saved successfully!!!")
+    enter = int(input("press 1 to enter or press 0 to save record: "))
+    condition = condition + press
+    press = press + enter
+print("Your record is saved successfully!!!")
