7ffdd373ba6df17512497e2d43424bfcbe5142fa
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-09 16:43:26 +0500

    List

--- Lecture/lec6/list.py
+++ Lecture/lec6/list.py
@@ -3,4 +3,10 @@
 std_list = ["abdullah","nisar","shifa","rehan","usama","ayesha"]
 for x in std_list:
     print(x)
+print("-----------------------\n\n")
+
+std_list = ["abdullah","nisar","shifa","rehan","usama","ayesha"]
+std_list.sort()
+for x in std_list:
+    print(x)
 print("-----------------------")
