d990c5fee1d805b5659da2874a8df5a8492445de
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-09 16:38:04 +0500

    List

--- Lecture/lec5/list_diff._functions.py
+++ Lecture/lec5/list_diff._functions.py
@@ -32,3 +32,12 @@ bsse_1A.insert(3,30)
 print(bsse_1A)
 print(bsse_2A)
 print("---------------------------------------")
+
+bsse_2A = [40, 200, 460, 900, 870]
+bsse_1A = bsse_2A.copy()
+bsse_1A.insert(3,30)
+bsse_2A.sort()
+bsse_1A.sort()
+print(bsse_1A)
+print(bsse_2A)
+print("---------------------------------------")
