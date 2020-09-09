98879c91671588d5affbb4bdfd7ffc96d43b78ed
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-09 19:11:58 +0500

    While loop for student list

--- Lecture/lec6/studentlist_whileloop_user.py
+++ Lecture/lec6/studentlist_whileloop_user.py
@@ -1,1 +1,21 @@
 # student list by while loop and user will tell condition
+
+std_roll_no = []
+std_name = []
+std_father_name = []
+std_section = []
+std_dep = []
+print("Student record system")
+print("-----------------------------------\n")
+condition = int(input("Tell how many student data you want to enter"))
+print("-----------------------------------\n")
+std_index = 0
+while (std_index < condition):
+    std_name.append(input("Enter name of new student: "))
+    std_father_name.append(input("Enter father name of new student: "))
+    std_roll_no.append(input("Enter roll no of new student: "))
+    std_section.append(input("Enter section of new student: "))
+    std_dep.append(input("Enter department of new student: "))
+    print("---------------------------------------------------------\n")
+    std_index = + 1
+print("Your record is saved successfully!!!")
