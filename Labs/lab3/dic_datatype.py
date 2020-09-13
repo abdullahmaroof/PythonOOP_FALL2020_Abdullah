399357538b40638b7dfa5f5df056f2cf829183c3
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-13 18:53:57 +0500

    Dictionary datatype

--- Labs/lab3/dic_datatype.py
+++ Labs/lab3/dic_datatype.py
@@ -13,3 +13,13 @@ print("Accessing an item from dictionary by get()\n")
 access = std1.get("Section")
 print(access)
 print("-------------------\n")
+
+print("Changing value from dictionary\n")
+std1["Roll No"] = "BSEM-S20-001"
+print(std1)
+print("-------------------\n")
+
+print("Using for loop in dictionary\n")
+for x in std1:
+    print(x)
+
