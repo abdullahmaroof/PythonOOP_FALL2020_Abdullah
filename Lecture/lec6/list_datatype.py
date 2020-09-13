5cf216a6ea8ec7c90d711c674168c644268f0fbd
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-13 21:53:36 +0500

    List Datatype

---
+++ Lecture/lec6/list_datatype.py
@@ -0,0 +1,7 @@
+# List datatype
+print("")
+print("Printing a list")
+std = ["Abdullah", "Ali", "Rehan", "Shifa", "Waqas", "Habiba"]
+print(std)
+print(type(std))
+print("-----------------------------------------------")
