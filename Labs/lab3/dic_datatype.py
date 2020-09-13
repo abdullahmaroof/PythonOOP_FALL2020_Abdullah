c181140a6e10100c4fcc13694cbbbf220941e91a
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-13 18:49:52 +0500

    Dictionary datatype

--- Labs/lab3/dic_datatype.py
+++ Labs/lab3/dic_datatype.py
@@ -2,3 +2,9 @@
 
 std1 = {"Name": "Abdullah Maroof", "Roll No": "BAIM-S20-001", "Section": "2A"}
 print(std1)
+print("-------------------\n")
+
+print("Accessing an item from dictionary\n")
+access = std1["Roll No"]
+print(access)
+print("-------------------\n")
