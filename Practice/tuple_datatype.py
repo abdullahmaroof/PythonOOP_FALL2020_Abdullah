95512732f529dbb11ce8287baef37a954a17d9c5
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-15 20:23:38 +0500

    Tuple datatype

---
+++ Practice/tuple_datatype.py
@@ -0,0 +1,7 @@
+#Tuple datatype
+
+print("\n-----Tuple datatype-----\n")
+std_name = ("Abdullah","Aneesa","Rehan","Shifa","Nisar","Sami","Waqas")
+print(std_name)
+print(type(std_name))
+print("-----------------------------------------")
