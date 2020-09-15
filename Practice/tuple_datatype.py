549534432240fd1e01c38f0c2e7d674ce6432e34
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-15 20:25:01 +0500

    Tuple datatype

--- Practice/tuple_datatype.py
+++ Practice/tuple_datatype.py
@@ -5,3 +5,10 @@ std_name = ("Abdullah","Aneesa","Rehan","Shifa","Nisar","Sami","Waqas")
 print(std_name)
 print(type(std_name))
 print("-----------------------------------------")
+
+print("\nAccessing tuple item by postive index\n")
+std_name = ("Abdullah","Aneesa","Rehan","Shifa","Nisar","Sami","Waqas")
+print(std_name[3])
+print("-----------------------------------------")
+
+
