2e3953efb6c1cb50f60e63fbacbdc733c136b293
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-15 12:43:18 +0500

    c++ oop lab assignment in python by suing class author

---
+++ Lecture/lec7/doctordata_class.py
@@ -0,0 +1,21 @@
+# use of class in python
+
+print("")
+print("------------c++ OPP Lab assignment in python------------")
+print("")
+class author:
+    name = ""
+    email = ""
+    gender = ""
+
+aut1 = authorr()
+aut1.name = "Aslam Siddque"
+aut1.email = "aslamsiddque12@gmail.com"
+aut1.gender = "Male"
+
+print("")
+print("-------Doctor data-------")
+print("\n\tName: ",aut1.name)
+print("\tEmail: ",aut1.email)
+print("\tGender: ",aut1.gender)
+print("\n----------------------------------------")
