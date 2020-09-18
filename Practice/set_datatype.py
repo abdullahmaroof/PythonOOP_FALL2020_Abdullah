34120e60a27f78fa6bf5b3b740023b1ab72e3b13
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-18 09:24:57 +0500

    set datatype

---
+++ Practice/set_datatype.py
@@ -0,0 +1,8 @@
+# set datatype
+
+set1 = {"Apple","Banna","Orange","Lemon"}
+print(set1)
+print(type(set1))
+print("------------------------------")
+print("Set is same like tuple but it doesn't have key for every item")
+print("-------------------------------------------------\n")
