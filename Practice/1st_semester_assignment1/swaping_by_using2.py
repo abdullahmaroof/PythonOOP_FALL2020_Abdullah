808e8efbe86fc515a1db222f60b28fe9db7d55ab
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-09-25 14:54:51 +0500

    Assignment 1 first semester

---
+++ Practice/1st_semester_assignment1/swaping_by_using2.py
@@ -0,0 +1,15 @@
+# swaping two values by using two values
+
+value1 = 12
+value2 = 15
+
+print("Before swaping")
+print(value1)
+print(value2)
+print("-----------------------------------")
+value1 = value2
+value2 = value1 - 3
+print("\nAfter Swapping")
+print(value1)
+print(value2)
+print("-----------------------------------")
