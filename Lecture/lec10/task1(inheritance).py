a9a264b07c70ec83c8cbacb1a5d443702071cdbf
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-10-01 06:54:00 +0500

    inheritance multiple

---
+++ Lecture/lec10/task1(inheritance).py
@@ -0,0 +1,11 @@
+# task1 of inheritance
+# multiple inheritance
+
+class parent:
+    father_name = ""
+    mother_name = ""
+
+    def motorcycle(self):
+        print("This is ", self.father_name,"'s motorcyle")
+
+class child:
