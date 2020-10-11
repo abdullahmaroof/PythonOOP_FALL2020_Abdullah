5567c9eb26b9c0b5f206beb9b60262ee87f73440
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-10-11 10:22:39 +0500

    AI voice assistant

--- AI_voice_assistant/AI_voice_assistant.py
+++ AI_voice_assistant/AI_voice_assistant.py
@@ -6,7 +6,7 @@ import wikipedia
 import webbrowser
 import os
 import random
-
+song = {"Dark side": "D:\\MUSICON\\english\\Alan Walker - Darkside.mp3"}
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voices',voices[0].id)
@@ -22,12 +22,12 @@ def wishme():
         speak('Good Afternoon!')
     else:
         speak('Good Evening!')
-    speak('I am AI machine sir, Please tell me how may i help you')
+    speak('I am AI machine sir')
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print('Listening...........')
-        r.pause_threshold = 1
+        r.pause_threshold = 0.5
         audio = r.listen(source)
     try:
         print('Recognizing............')
@@ -40,32 +40,56 @@ def takeCommand():
     return query
 
 wishme()
-while True:
+speak("Sir tell me your name for verfication")
+query = takeCommand().lower()
+if 'abdullah' in query:
+    speak("Sir abdullah, please tell your verification code")
     query = takeCommand().lower()
-    if 'wikipedia' in query:
-        print("Sreaching Wikipedia")
-        query = query.replace("Wikipedia", "")
-        results = wikipedia.summary(query, sentences=5)
-        speak("According to Wikipedia")
-        print(results)
-        speak(results)
+    if '1101' in query:
+        speak("Sir abdullah, thank you for verification. pkease tell me how may i help you")
+        #while True:
+        if 1:
+            query = takeCommand().lower()
+            if 'wikipedia' in query:
+                print("Sreaching Wikipedia")
+                query = query.replace("Wikipedia", "")
+                results = wikipedia.summary(query, sentences=5)
+                speak("According to Wikipedia")
+                print(results)
+                speak(results)
 
-    elif 'open youtube' in query:
-        webbrowser.open("youtube.com")
+            elif 'open youtube' in query:
+                webbrowser.open("youtube.com")
 
-    elif 'open facebook' in query:
-        webbrowser.open("facebook.com")
+            elif 'open facebook' in query:
+                webbrowser.open("facebook.com")
 
-    elif 'open gaana' in query:
-        webbrowser.open("gaana.com")
+            elif 'open ghana' in query:
+                webbrowser.open("gaana.com")
 
-    elif 'open google' in query:
-        webbrowser.open("google.com")
+            elif 'open google' in query:
+                webbrowser.open("google.com")
 
-    elif 'open github' in query:
-        webbrowser.open("github.com")
-    elif 'play music' in query:
-        music_dir = "D:\\MUSICON\\trending audio"
-        songs = os.listdir(music_dir)
-        print(songs)
-        os.startfile(os.path.join(music_dir, songs[5]))
+            elif 'open github' in query:
+                webbrowser.open("github.com")
+
+            elif 'open stack over flow' in query:
+                webbrowser.open("stackoverflow.com")
+
+            elif 'open zoom' in query:
+                webbrowser.open("zoom.com")
+
+            elif 'play music' in query:
+                music_dir = "D:\\MUSICON\\trending audio"
+                songs = os.listdir(music_dir)
+                print(songs)
+                os.startfile(os.path.join(music_dir, songs[8]))
+
+            elif 'time' in query:
+                strTime = datetime.datetime.now().strftime("%H:%M:%S")
+                print(f"Sir, the time is {strTime}")
+                speak(f"Sir, the time is {strTime}")
+
+            elif 'open python' in query:
+                pathPYcharm = "C:\\Program Files\\JetBrains\\PyCharm Community Edition 2020.2\\bin\\pycharm64.exe"
+                os.startfile(pathPYcharm)
