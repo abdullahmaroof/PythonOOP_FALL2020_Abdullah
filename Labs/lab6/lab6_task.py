841001a86ff5ff7b3d1f791b85f788c0bf0f8b25
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-10-15 10:49:49 +0500

    Constructor overloading

--- Labs/lab6/lab6_task.py
+++ Labs/lab6/lab6_task.py
@@ -1,17 +1,19 @@
 class Fruit:
     name = ""
     color = ""
-    def __init__(self,fname,fcolor):
+    taste = ""
+    def __init__(self,fname,fcolor,ftaste):
         self.name = fname
         self.color = fcolor
+        self.taste = ftaste
 
     def introduceFruit(self):
         print("Fruit name is ", self.name)
         print("Fruit Color", self.color)
 
 class winterFruit(Fruit):
-    def fruitTaste(self, taste):
-        print("The taste of winter fruit is ", taste)
+    def fruitTaste(self):
+        print("The taste of winter fruit is ", self.taste)
 
     def introduceFruit(self):
         print("This is  winter fruit")
@@ -20,8 +22,8 @@ class winterFruit(Fruit):
 
 
 class summerFruit(Fruit):
-    def fruitTaste(self, taste):
-        print("The taste of summer fruit is ", taste)
+    def fruitTaste(self):
+        print("The taste of summer fruit is ", self.taste)
 
     def introduceFruit(self):
         print("This is s summer fruit")
@@ -29,22 +31,22 @@ class summerFruit(Fruit):
         print("Fruit Color", self.color)
 
 
-f1 = Fruit("Banana", "Yellow")
+f1 = Fruit("Banana", "Yellow", "Sweet")
 f1.introduceFruit()
 print("")
-sf1 = summerFruit("Mango","Yellow")
+sf1 = summerFruit("Mango","Yellow","Sweet")
 sf1.introduceFruit()
-sf1.fruitTaste("sweeter")
+sf1.fruitTaste()
 print("")
-sf2 = summerFruit("Apple", "Red")
+sf2 = summerFruit("Apple", "Red","Sweet")
 sf2.introduceFruit()
-sf2.fruitTaste("sweet")
+sf2.fruitTaste()
 print("")
-w1 = winterFruit("Orange","Orange")
+w1 = winterFruit("Orange","Orange","Sweet")
 w1.introduceFruit()
-w1.fruitTaste("sweet")
+w1.fruitTaste()
 print("")
-w2 = winterFruit("Graps","Soar")
+w2 = winterFruit("Graps","Purple","Soar")
 w2.introduceFruit()
-w2.fruitTaste("Soar")
+w2.fruitTaste()
 
