000788719f9d303253b4aa554e894c60f1abf2df
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-11-07 18:35:38 +0500

    Project GUI

--- Practice/Project_GUI/Welcome_GUI.py
+++ Practice/Project_GUI/Welcome_GUI.py
@@ -2,15 +2,15 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 root = Tk()
-root.geometry("800x600")
+root.geometry("800x370")
 root.title("Online Shopping System")
 root.iconbitmap(r'buy_online_5Wq_icon.ico')
 
 
 #*****************Upper part***************
 
-Upper_root = Frame(root,bg="cornflower blue", height=100)
-Title_upper_root = Label(Upper_root, text="Online Shopping System", bg="cornflower blue", pady=10)
+Upper_root = Frame(root,bg="cornflower blue", height=50)
+Title_upper_root = Label(Upper_root, text="Online Shopping System", bg="cornflower blue", pady=10, font=('arial',28,'bold'))
 
 
 Upper_root.pack(side= TOP, fill= X)
@@ -20,9 +20,11 @@ Title_upper_root.pack()
 
 
 #*****************Center part*******************
-Center_root = Frame(root,bg="gray70", height=400)
-part1_Center_root = Frame(Center_root, bg="gray70", height=170)
-part2_Center_root = Frame(Center_root, bg="blue", height=230)
+Center_root = Frame(root,bg="gray92", height=320)
+part1_Center_root = Frame(Center_root, bg="gray92", height=170)
+part2_Center_root = Frame(Center_root, bg="gray92", height=150)
+fbutton1_Cnter_root = Frame(part2_Center_root, bg="blue", width=400, height=150)
+fbutton2_Cnter_root = Frame(part2_Center_root, bg="red", width=400, height=150)
 pic = Image.open("shopping-bag.png")
 resized = pic.resize((150, 100), Image.ANTIALIAS)
 mypic = ImageTk.PhotoImage(resized)
@@ -31,13 +33,11 @@ Center_root.pack(side= TOP, fill= X)
 part1_Center_root.pack(side=TOP, fill=X)
 image_upper_root.pack(pady=10)
 part2_Center_root.pack(side= BOTTOM, expand=1, fill=BOTH)
-
-#*****************bottom part*******************
-Bottom_root = Frame(root,bg="cornflower blue", height=70)
-Bottom_root.pack(side= TOP, fill= X)
+fbutton1_Cnter_root.pack(side=LEFT, fill=BOTH)
+fbutton2_Cnter_root.pack(side=LEFT, fill=BOTH)
 
 #*****************end part*******************
-Bottom_root = Frame(root,bg="light grey", height=30)
+Bottom_root = Frame(root,bg="cornflower blue", height=30)
 Bottom_root.pack(side= BOTTOM, fill= X)
 
 mainloop()
