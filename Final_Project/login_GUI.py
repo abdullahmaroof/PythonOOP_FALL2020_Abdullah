6aa2a94e5099588a4eae409d70b1d82b4c5ec991
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-11-21 17:33:21 +0500

    Project

--- Final_Project/login_GUI.py
+++ Final_Project/login_GUI.py
@@ -2,7 +2,7 @@ from tkinter import *
 from PIL import ImageTk, Image
 root = Tk()
 
-root.geometry("800x450")
+root.geometry("800x550")
 root.title('Online Food Order System')
 root.iconbitmap(r'buy_online_5Wq_icon.ico')
 #*****************upper block*****************************
@@ -13,21 +13,35 @@ upper_Root.pack(side=TOP, fill=BOTH, expand=1)
 title.pack(pady=30)
 
 #*****************upper block*****************************
-center_Root = Frame(root, bg="gray92", height=300)
+center_Root = Frame(root, bg="gray92", height=400)
 top_center = Frame(center_Root, bg="gray92", height=140)
 pic = Image.open("shopping-bag.png")
 resized = pic.resize((150, 100), Image.ANTIALIAS)
 mypic = ImageTk.PhotoImage(resized)
 image_top_center = Label(top_center, image = mypic)
 center_center = Frame(center_Root, bg="gray92", height=80)
+sub_title = Label(center_center, text="LOGIN", bg="gray92", font=('arial',20,'underline','bold'))
 bottom_center = Frame(center_Root, bg="gray92", height=180)
+top_botcenter = Frame(bottom_center, bg="gray92", height=100)
+left_top_bot = Frame(top_botcenter, height=100, bg="gray92")
+name_label = Label(left_top_bot, text="User Name:  ", font=('arial',12,'bold'), anchor=E, bg="gray92")
+pass_label = Label(left_top_bot, text="Password:  ", font=('arial',12,'bold'), anchor=E, bg="gray92")
+right_top_bot = Frame(top_botcenter, height=100, bg="yellow")
+bottom_botcenter = Frame(bottom_center, bg="blue", height=80)
 
 
 center_Root.pack(side=TOP, fill=BOTH, expand=1)
 top_center.pack(side=TOP, fill=BOTH, expand=1)
 image_top_center.pack(pady=20)
 center_center.pack(side=TOP, fill=BOTH, expand=1)
+sub_title.pack(pady=10)
 bottom_center.pack(side=TOP, fill=BOTH, expand=1)
+top_botcenter.pack(side=TOP, fill=BOTH, expand=1)
+left_top_bot.pack(side=LEFT, fill=BOTH, expand=1)
+name_label.pack(pady=10, fill=BOTH, expand=1)
+pass_label.pack(pady=10, fill=BOTH, expand=1)
+right_top_bot.pack(side=LEFT, fill=BOTH, expand=1)
+bottom_botcenter.pack(side=TOP, fill=BOTH, expand=1)
 
 
 #*****************upper block*****************************
@@ -35,7 +49,7 @@ bottom_Root = Frame(root, bg="light blue", height=50)
 copyRight = Label(bottom_Root, text=" © Copy Rights Reserved, Abdullah Maroof 2020", bg="light blue", font=('arial',12,'bold'))
 
 bottom_Root.pack(side=BOTTOM, fill=BOTH, expand=1)
-copyRight.pack()
+copyRight.pack(pady=10)
 
 mainloop()
 
