55c6c16f8c8b4da570c0ef1c3d67bb50da5f6942
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-11-21 16:50:50 +0500

    Project

--- Final_Project/login_GUI.py
+++ Final_Project/login_GUI.py
@@ -13,30 +13,21 @@ upper_Root.pack(side=TOP, fill=BOTH, expand=1)
 title.pack(pady=30)
 
 #*****************upper block*****************************
-center_Root = Frame(root, bg="gray92", height=320)
+center_Root = Frame(root, bg="gray92", height=300)
 top_center = Frame(center_Root, bg="gray92", height=140)
 pic = Image.open("shopping-bag.png")
 resized = pic.resize((150, 100), Image.ANTIALIAS)
 mypic = ImageTk.PhotoImage(resized)
 image_top_center = Label(top_center, image = mypic)
 center_center = Frame(center_Root, bg="gray92", height=80)
-left_center_cenroot = Frame(center_center, bg="gray92", height=80)
-login_but = Button(left_center_cenroot, text="LOGIN", width=25, height=2, bg="light blue", font=('arial',10,'bold'), activebackground="black", activeforeground="white")
-right_center_cenroot = Frame(center_center, bg="gray92", height=80)
-signup_but = Button(right_center_cenroot, text="SIGNUP", width=25, height=2, bg="light blue", font=('arial',10,'bold'), activebackground="black", activeforeground="white")
-bottom_center = Frame(center_Root, bg="gray92", height=80)
-loginadmin_but = Button(bottom_center, text="LOGIN As ADMIN", width=25, height=2, bg="light blue", font=('arial',10,'bold'), activebackground="black", activeforeground="white")
+bottom_center = Frame(center_Root, bg="gray92", height=180)
+
 
 center_Root.pack(side=TOP, fill=BOTH, expand=1)
 top_center.pack(side=TOP, fill=BOTH, expand=1)
 image_top_center.pack(pady=20)
 center_center.pack(side=TOP, fill=BOTH, expand=1)
-left_center_cenroot.pack(side=LEFT, fill=BOTH, expand=1)
-login_but.pack(pady=20)
-right_center_cenroot.pack(side=LEFT, fill=BOTH, expand=1)
-signup_but.pack(pady=20)
 bottom_center.pack(side=TOP, fill=BOTH, expand=1)
-loginadmin_but.pack(pady=20)
 
 
 #*****************upper block*****************************
