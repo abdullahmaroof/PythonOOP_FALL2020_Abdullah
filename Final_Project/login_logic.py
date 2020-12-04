03a0717b05ea72f800bc55ebba59a7997efd59d6
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-12-04 17:07:24 +0500

    Final Project

--- Final_Project/login_logic.py
+++
@@ -1,7 +0,0 @@
-from Final_Project.systemmain_GUI import call_system
-
-username = ""
-password = ""
-def call_mainsystem():
-    if username == "abdullah" and password == 12345:
-        call_system()
