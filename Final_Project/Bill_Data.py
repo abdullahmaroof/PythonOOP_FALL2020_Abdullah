8e0d5bd7edf8d5bc97ffff1fb33044090fbc5799
abdullahmaroof <abdullahmaroof597@gmail.com>
2020-12-06 12:26:55 +0500

    Final Project

---
+++ Final_Project/Bill_Data.py
@@ -0,0 +1,115 @@
+from random import *
+
+class for_bill:
+    rand_value = randint(1000, 4000)
+    food = ""
+    flavour = ""
+    price = ""
+    note = ""
+
+
+class spizza_chf(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Chicken Fajita"
+    price = 300
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class mpizza_chf(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Chicken Fajita"
+    price = 500
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class lpizza_chf(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Chicken Fajita"
+    price = 800
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class spizza_peeri(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Peeri Peeri"
+    price = 350
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class mpizza_peeri(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Peeri Peeri"
+    price = 600
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class lpizza_peeri(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Smoke Mexico"
+    price = 900
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+
+class spizza_m(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Smoke Mexico"
+    price = 500
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class mpizza_m(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Smoke Mexico"
+    price = 800
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class lpizza_m(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Smoke Mexico"
+    price = 1100
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class spizza_ch(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Cheese"
+    price = 320
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class mpizza_ch(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Cheese"
+    price = 500
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class lpizza_ch(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Cheese"
+    price = 950
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class spizza_bbq(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Bar BQ"
+    price = 350
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class mpizza_bbq(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Bar BQ"
+    price = 600
+    note = "Your order will be serve in 20 min. Thank you!!!"
+
+class lpizza_bbq(for_bill):
+    rand_value = randint(1000, 4000)
+    food = "Pizza"
+    flavour = "Bar BQ"
+    price = 1000
+    note = "Your order will be serve in 20 min. Thank you!!!"
