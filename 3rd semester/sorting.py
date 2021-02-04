7e92b047a5a0784441a09be064473a0f5c07cffc
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-02-04 10:33:47 +0500

    sort

---
+++ 3rd semester/sorting.py
@@ -0,0 +1,11 @@
+
+sort = [12,4,14,87,45]
+n=5;
+for i in range(0,n-1):
+    for j in range(0,n-i-1):
+        if(sort[j]>sort[j+1]):
+            temp = sort[j]
+            sort[j] = sort[j+1]
+            sort[j+1] = temp
+
+print(sort)
