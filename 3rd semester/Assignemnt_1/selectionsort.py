18742f81cbd34b247db8301af4dab408913ee73e
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-02-13 14:37:55 +0500

    selection sort

---
+++ 3rd semester/Assignemnt_1/selectionsort.py
@@ -0,0 +1,17 @@
+#get data from file
+marks_list = []
+file = open("marks.txt","r")
+for marks in file:
+    marks_list.append(int(marks))
+print("Marks list: ",marks_list)
+n= len(marks_list)
+#bubblesort
+for i in range(0,n-1):
+    min = i
+    for j in range(i,n):
+        if(marks_list[j]<marks_list[min]):
+            min = j
+    temp = marks_list[i]
+    marks_list[i] = marks_list[min]
+    marks_list[min] = temp
+print("\nBubble sorted list: ",marks_list)
