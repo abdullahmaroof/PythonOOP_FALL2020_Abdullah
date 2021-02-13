49dc8cf1ad700316a96fc88d7ff72ca9a91aff0a
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-02-13 14:35:00 +0500

    bubble sort

--- 3rd semester/Assignemnt_1/bubblesort.py
+++ 3rd semester/Assignemnt_1/bubblesort.py
@@ -5,6 +5,7 @@ for marks in file:
     marks_list.append(int(marks))
 print("Marks list: ",marks_list)
 n= len(marks_list)
+#bubblesort
 for i in range(0,n-1):
     for j in range(0,n-i-1):
         if(marks_list[j]>marks_list[j+1]):
