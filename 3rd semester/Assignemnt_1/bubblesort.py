7c506e3179e8c8d2dc65fccca492c28630305e7a
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-02-13 14:33:06 +0500

    bubble sort

--- 3rd semester/Assignemnt_1/bubblesort.py
+++ 3rd semester/Assignemnt_1/bubblesort.py
@@ -2,5 +2,13 @@
 marks_list = []
 file = open("marks.txt","r")
 for marks in file:
-    marks_list.append(marks)
+    marks_list.append(int(marks))
 print("Marks list: ",marks_list)
+n= len(marks_list)
+for i in range(0,n-1):
+    for j in range(0,n-i-1):
+        if(marks_list[j]>marks_list[j+1]):
+            temp = marks_list[j]
+            marks_list[j] = marks_list[j+1]
+            marks_list[j+1] = temp
+print("\nBubble sorted list: ",marks_list)
