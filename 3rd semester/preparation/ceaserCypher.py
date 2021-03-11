795f4a164dd35603f943abf882fc5caf66fff5d5
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-03-11 23:48:48 +0500

    exam

--- 3rd semester/preparation/ceaserCypher.py
+++ 3rd semester/preparation/ceaserCypher.py
@@ -1,11 +1,29 @@
 data = input("Enter data to implement ceaserCypher:  ")
 shift = int(input("Enter the value for shift: "))
-for x in range(0,len(data)):
-    if data[x].isupper():
-        print(data[x])
-    elif data[x].islower():
-        print(data[x])
-    elif data[x].isdecimal():
-        print(data[x])
+newdata = ""
+for x in data:
+    if x.islower():
+        if (ord(x) + shift) <= 122:
+            e = chr(ord(x) + shift)
+            newdata = newdata + e
+        else:
+            e = chr((ord(x) + shift) - 122 + 97)
+            newdata = newdata + e
+    elif x.isupper():
+        if (ord(x)+shift) <= 90:
+            e = chr(ord(x) + shift)
+            newdata = newdata + e
+        else:
+            e = chr((ord(x)+shift)-90+64)
+            newdata = newdata + e
+    elif x.isdecimal():
+        if (int(x)+shift) <= 9:
+            e = int(x) + shift
+            newdata = newdata + str(e)
+        else:
+            e = (int(x)+shift)-9
+            newdata = newdata + str(e)
     else:
-        print("space")
+        newdata = newdata + x
+
+print(newdata)
