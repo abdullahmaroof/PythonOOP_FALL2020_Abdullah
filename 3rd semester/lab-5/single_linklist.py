aebf6d33e8592bffbc46e8eafd6bfb107ec02e9a
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-03-31 18:28:57 +0500

    Single-linklist

--- 3rd semester/lab-5/single_linklist.py
+++ 3rd semester/lab-5/single_linklist.py
@@ -11,8 +11,29 @@ class single_linklist:
         if self.head is None:
             print("Single Linklist is empty")
         else:
-            pass
+            point = self.head
+            linklist = ""
+            while point:
+                linklist += str(point.data)+" --> "
+                point = point.next
+            print(linklist)
+
+    def insert_at_start(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+        else:
+            point = self.head
+            while point.next:
+                point = point.next
+            point.next = Node(data,None)
 
 
 list = single_linklist()
+list.insert_at_start(12)
+list.insert_at_start(32)
+list.insert_at_end(89)
 list.printlist()
