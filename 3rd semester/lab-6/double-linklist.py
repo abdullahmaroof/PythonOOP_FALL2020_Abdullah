e0cd0c9f91f71002545894ce603cc4b530547738
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-01 21:29:16 +0500

    double linklist

--- 3rd semester/lab-6/double-linklist.py
+++ 3rd semester/lab-6/double-linklist.py
@@ -1,6 +1,6 @@
 class Node:
-    def __init__(self, pervious=None, data=None, next=None):
-        self.previous = pervious
+    def __init__(self, previous=None, data=None, next=None):
+        self.previous = previous
         self.data = data
         self.next = next
 
@@ -19,18 +19,19 @@ class double_linklist:
                 point = point.next
             print(linklist)
 
-    def printlist_reverse(self):
-        if self.head is None:
-            print("double Linklist is empty")
-        else:
-            point = self.head
-            linklist = ""
-            while point.next is not None:
-                point = point.next
-            while point is not  None:
-                linklist += str(point.data) + " --> "
-                point = point.pervious
-            print(linklist)
+    #def printlist_reverse(self):
+     #   if self.head is None:
+      #      print("double Linklist is empty")
+      #  else:
+       #     point = self.head
+        #    linklist = ""
+        #    while point.next is not None:
+        #        point = point.next
+        #    while point is not None:
+        #        linklist += str(point.data)+" --> "
+        #        point = point.previous
+
+        #    print(linklist)
 
     def tell_length(self):
         length = 0
@@ -40,6 +41,30 @@ class double_linklist:
             point = point.next
         return length
 
+    def insert_at_start(self, data):
+        node = Node(self.head,data,self.head)
+        self.head = node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(None,data, None)
+        else:
+            point = self.head
+            while point.next:
+                point = point.next
+            point.next = Node(point,data,None)
+
+
+
+
 
 list = double_linklist()
+list.insert_at_start(12)
+list.insert_at_start(23)
+list.insert_at_start(32)
+list.insert_at_start(11)
 print(list.tell_length())
+list.printlist_forward()
+list.insert_at_end(100)
+list.printlist_forward()
+
