17f5ae6844185966f6583bfb312eee8c9b16a427
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-01 22:54:40 +0500

    numpy

--- 3rd semester/numpy_1.py
+++ 3rd semester/numpy_1.py
@@ -3,9 +3,6 @@ import numpy as np
 arr = np.array((
     [1,2,3,4,5],
     [2,4,6,8,10],
-    [0,2,4,6,8],
-    [1,3,5,7,9],
-    [3,6,9,12,15]
 ))
 
 print(arr)
@@ -21,3 +18,6 @@ arr1 = arr.copy()
 print("after doing deep copying")
 print(id(arr))
 print(id(arr1))
+arr2 = arr.reshape(5,2)
+print(arr2)
+
