97664eefff699d9ca2c311e9373e9cda1277ce20
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-02 09:47:46 +0500

    numpy matrix

--- 3rd semester/numpy_3(matrix).py
+++ 3rd semester/numpy_3(matrix).py
@@ -2,10 +2,10 @@ import numpy as np
 
 arr1 = np.arange(25).reshape(5,5)
 print(arr1)
-arr2 = np.array(([0,0],[0,0]))
 print("Taking Transpose of matrix")
 arr2 = arr1.transpose()
 print(arr2)
 print("dot product:")
 arr3 = np.dot(arr1,arr2)
 print(arr3)
+
