f907873fd9489d8298263a7395e745ffb3ed8d01
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-02 19:06:32 +0500

    numpy practice

---
+++ thirdsemester/lab-7/numpy_prac.py
@@ -0,0 +1,11 @@
+import numpy as math
+
+arr = math.array([1,3,5,7,8])
+print("1d array of numpy: ")
+print(arr)
+arr1 = math.array(([1,3,5,7,8],[4,6,9,10,2],[3,0,11,7,4]))
+print("\n2d array of numpy: ")
+print(arr1)
+arr2 = math.array((([1,3,5,7,8],[4,6,9,10,2]),([1,2,3,4,8],[3,0,11,7,4])))
+print("\n3d array of numpy: ")
+print(arr2)
