e5adc405ba65d8265c9ee218b2a0d0c8f8f8f2bf
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-02 19:08:46 +0500

    numpy practice

--- thirdsemester/lab-7/numpy_prac.py
+++ thirdsemester/lab-7/numpy_prac.py
@@ -3,9 +3,18 @@ import numpy as math
 arr = math.array([1,3,5,7,8])
 print("1d array of numpy: ")
 print(arr)
+print("Division 1d array of numpy with 2: ")
+print(arr/2)
+
 arr1 = math.array(([1,3,5,7,8],[4,6,9,10,2],[3,0,11,7,4]))
-print("\n2d array of numpy: ")
+print("\n\n2d array of numpy: ")
 print(arr1)
+print("Division 2d array of numpy with 2: ")
+print(arr1/2)
+
 arr2 = math.array((([1,3,5,7,8],[4,6,9,10,2]),([1,2,3,4,8],[3,0,11,7,4])))
-print("\n3d array of numpy: ")
+print("\n\n3d array of numpy: ")
 print(arr2)
+print("Division 3d array of numpy with 2: ")
+print(arr2/2)
+
