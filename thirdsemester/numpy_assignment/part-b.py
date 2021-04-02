6894c27e5c10064af1cea3fe2ea6c07351eb3abd
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-02 10:22:12 +0500

    numpy assignment

--- thirdsemester/numpy_assignment/part-b.py
+++ thirdsemester/numpy_assignment/part-b.py
@@ -2,3 +2,7 @@ from thirdsemester.numpy_assignment.part_a import *
 import numpy as np
 new_arr3 = arr3
 new_arr4 = arr4
+print("\nslicing arr3 in 3x3 by new_arr3")
+print(new_arr3[0:3,0:3])
+print("\nslicing arr4 in 3x3 by new_arr4")
+print(new_arr4[0:3,0:3])
