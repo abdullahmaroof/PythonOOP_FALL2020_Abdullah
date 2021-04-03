6692a62d155985ef602c40986a7a3f532c759ce1
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-03 12:41:36 +0500

    numpy practice

---
+++ thirdsemester/lab-7/numpy(indexing)_prac.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+print("finding value in 1d from indexing, \npositive index start from 0, forward direction \nand negative index start from -1, reverse direction:")
+onedarr = np.array([2,4,6,7,8,23,12,56,32])
+print("1 D array: ",onedarr)
+print("poitive index 3: ",onedarr[3])
+print("negative index -4: ",onedarr[-4])
