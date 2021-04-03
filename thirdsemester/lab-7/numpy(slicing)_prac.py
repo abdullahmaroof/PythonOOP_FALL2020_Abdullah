aa10366e4ae939d261c8f12a31666f68a169f6ff
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-03 13:07:53 +0500

    numpy practice

---
+++ thirdsemester/lab-7/numpy(slicing)_prac.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+print("slicing of 1 D array, method arrayname[start:end range(it must be one more value:step(optional)")
+onedarr = np.array([1,3,5,7,12,34,21])
+print(onedarr)
+print("slicing method \'[:]\' :",onedarr[:])
