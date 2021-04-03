86b5965882ad3e860de6a7838b5a1b92de44c640
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-03 12:02:04 +0500

    numpy practice

---
+++ thirdsemester/lab-7/numpy(attributes)_prac.py
@@ -0,0 +1,6 @@
+import numpy as np
+print("We will check dimensional of array by using ndim")
+onedarr = np.array([1,2,3])
+print("dimension of",onedarr," : ",onedarr.ndim)
+twodarr = np.array(([1,2,3],[4,5,6],[8,7,9]))
+print("dimension of",twodarr," : ",twodarr.ndim)
