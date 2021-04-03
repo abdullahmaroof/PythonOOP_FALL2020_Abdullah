aa257af8ae05fe602906748c4e9897e2126e5703
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-03 12:47:26 +0500

    numpy practice

--- thirdsemester/lab-7/numpy(indexing)_prac.py
+++ thirdsemester/lab-7/numpy(indexing)_prac.py
@@ -5,3 +5,11 @@ onedarr = np.array([2,4,6,7,8,23,12,56,32])
 print("1 D array: ",onedarr)
 print("poitive index 3: ",onedarr[3])
 print("negative index -4: ",onedarr[-4])
+
+print("finding value in 2d from indexing and method is arrayname[row][column],"
+      " \npositive index start from 0, forward direction coloumn and downward direction row"
+      "\nand negative index start from -1, reverse direction column and upward direction row:")
+twodarr = np.array(([2,4,6],[8,23,12],[56,32,43]))
+print("1 D array: ",twodarr)
+print("poitive index row=1,coloum=2: ",twodarr[1][2])
+print("negative index row=-3,column=-1: ",twodarr[-3][-1])
