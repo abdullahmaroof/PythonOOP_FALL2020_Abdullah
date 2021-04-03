db5413ca5ffdf60c95bf6acb6fa978aec720bf8f
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-03 22:06:05 +0500

    numpy practice slicing

--- thirdsemester/lab-7/numpy(slicing)_prac.py
+++ thirdsemester/lab-7/numpy(slicing)_prac.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-print("slicing of 1 D array, method arrayname[start:end range(it must be one more value:step(optional)]")
+print("slicing of 1 D array, method arrayname[start:end range(it must be one more value):step(optional)]")
 onedarr = np.array([1,3,5,7,12,34,21])
 print("1 D array: ",onedarr)
 print("slicing method \'[:]\' :",onedarr[:])
@@ -10,8 +10,20 @@ print("slicing method \'[3:6]\' :",onedarr[3:6])
 print("slicing method \'[1:7:2]\' :",onedarr[1:7:2])
 
 print("slicing of 2 D array, "
-      "\nmethod arrayname[(row)start:end range(it must be one more value:step(optional),"
-      "(column)start:end range(it must be one more value:step(optional)]")
+      "\nmethod arrayname[(row)start:end range(it must be one more value):step(optional),"
+      "(column)start:end range(it must be one more value):step(optional)]")
+twodarr = np.array(([1,3,5,7],[12,34,21,78],[5,32,11,78],[58,96,66,45]))
+print("2 D array: ",twodarr)
+print("slicing method \'[:,:]\' :",twodarr[:,:])
+print("slicing method \'[2:,1:]\' :",twodarr[2:,1:])
+print("slicing method \'[:2,:2]\' :",twodarr[:2,:2])
+print("slicing method \'[1:3,0:3]\' :",twodarr[1:3,0:3])
+print("slicing method \'[0:4:2,0:4:2]\' :",twodarr[0:4:2,0:4:2])
+
+print("slicing of 3 D array, "
+      "\nmethod arrayname[(2 D array)start: end range(it must be one more value):step(optional),"
+      "(row)start:end range(it must be one more value):step(optional),"
+      "(column)start:end range(it must be one more value):step(optional)]")
 twodarr = np.array(([1,3,5,7],[12,34,21,78],[5,32,11,78],[58,96,66,45]))
 print("2 D array: ",twodarr)
 print("slicing method \'[:,:]\' :",twodarr[:,:])
