32a7f6e026ee40437ef19e0f894112feb4dcf770
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-03 13:09:59 +0500

    numpy practice

--- thirdsemester/lab-7/numpy(slicing)_prac.py
+++ thirdsemester/lab-7/numpy(slicing)_prac.py
@@ -2,7 +2,9 @@ import numpy as np
 
 print("slicing of 1 D array, method arrayname[start:end range(it must be one more value:step(optional)")
 onedarr = np.array([1,3,5,7,12,34,21])
-print(onedarr)
+print("1 D array: ",onedarr)
 print("slicing method \'[:]\' :",onedarr[:])
 print("slicing method \'[2:]\' :",onedarr[2:])
 print("slicing method \'[:4]\' :",onedarr[:4])
+print("slicing method \'[3:6]\' :",onedarr[3:6])
+print("slicing method \'[1:7:2]\' :",onedarr[1:7:2])
