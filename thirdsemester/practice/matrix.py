e9af076118a5030366a393c7422b297c9a78cff0
abdullahmaroof <abdullahmaroof597@gmail.com>
2021-04-04 11:49:22 +0500

    making matrix

--- thirdsemester/practice/matrix.py
+++ thirdsemester/practice/matrix.py
@@ -2,11 +2,29 @@ import numpy as np
 
 print("Make a matrix")
 row = int(input("1: Enter number of rows: "))
-column = int(input("1: Enter number of columns: "))
+column = int(input("2: Enter number of columns: "))
 no = row*column
 matrix = np.arange(no)
-for i in range(no):
-    value = int(input("Enter a value in matrix at index :"))
-    matrix[i] = value
-matrix = matrix.reshape(row,column)
-print(matrix)
+print("\nSelect the type of matrix:")
+print("1: Null Matrix")
+print("2: Ones Matrix")
+print("3: Matrix containing different values")
+press = int(input("Press 1 - 3: "))
+if press == 1:
+    matrix = np.zeros([row,column],dtype="int")
+    print("\n\nZero Matrix:")
+    print(matrix)
+elif press == 2:
+    matrix = np.ones([row,column],dtype="int")
+    print("\n\nOnes Matrix:")
+    print(matrix)
+elif press == 3:
+    print("\n")
+    for i in range(no):
+        value = int(input("Enter a value in matrix at index :"))
+        matrix[i] = value
+    matrix = matrix.reshape(row,column)
+    print("Matrix:")
+    print(matrix)
+else:
+    print("\n\nWrong key")
